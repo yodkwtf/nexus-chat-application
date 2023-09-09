b5b1412aa1e2dec78ed20ae31fb25765c310319a
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2023-09-09 23:11:49 +0530

    mobile footer actions setup

---
+++ app/components/sidebar/MobileFooter.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import useConversation from '@/app/hooks/useConversation';
+import useRoutes from '@/app/hooks/useRoutes';
+import MobileFooterItem from './MobileFooterItem';
+
+const MobileFooter = () => {
+  const routes = useRoutes();
+  const { isOpen } = useConversation();
+
+  if (isOpen) {
+    return null;
+  }
+
+  return (
+    <div className="fixed justify-between w-full bottom-0 z-40 flex items-center bg-white border-t-[1px] lg:hidden">
+      {routes.map((route) => (
+        <MobileFooterItem
+          key={route.label}
+          href={route.href}
+          icon={route.icon}
+          active={route.active}
+          onClick={route.onClick}
+        />
+      ))}
+    </div>
+  );
+};
+export default MobileFooter;
---
+++ app/components/sidebar/MobileFooterItem.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import clsx from 'clsx';
+import Link from 'next/link';
+
+interface MobileFooterItemProps {
+  href: string;
+  icon: any;
+  active?: boolean;
+  onClick?: () => void;
+}
+
+const MobileFooterItem: React.FC<MobileFooterItemProps> = ({
+  icon: Icon,
+  href,
+  active,
+  onClick,
+}) => {
+  const handleClick = () => {
+    if (onClick) {
+      onClick();
+    }
+  };
+
+  return (
+    <Link
+      href={href}
+      onClick={onClick}
+      className={clsx(
+        'group flex gap-x-3 text-sm leading-6 font-semibold w-full justify-center p-4 text-gray-500 hover:text-black hover:bg-gray-100',
+        active && 'bg-gray-100 text-black'
+      )}
+    >
+      <Icon className="h-6 w-6" />
+    </Link>
+  );
+};
+export default MobileFooterItem;

f617064e7c8f97f20d6c130f1d3fe76169670a8a
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2023-09-16 21:25:02 +0530

    update profile name & images - UI & API

---
+++ app/api/settings/route.ts
@@ -0,0 +1,33 @@
+import getCurrentUser from '@/app/actions/getCurrentUser';
+import { NextResponse } from 'next/server';
+import prisma from '@/app/libs/prismadb';
+
+export async function POST(request: Request) {
+  try {
+    const currentUser = await getCurrentUser();
+    const body = await request.json();
+    const { name, image } = body;
+
+    // check current user
+    if (!currentUser) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    // update user
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: currentUser.id,
+      },
+      data: {
+        name,
+        image,
+      },
+    });
+
+    // return updated user
+    return NextResponse.json(updatedUser, { status: 200 });
+  } catch (error) {
+    console.log(error, 'ERROR_SETTINGS');
+    return new NextResponse('Internal Server Error', { status: 500 });
+  }
+}
--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -4,6 +4,7 @@ import { User } from '@prisma/client';
 import useRoutes from '@/app/hooks/useRoutes';
 import DesktopSidebarItem from './DesktopSidebarItem';
 import Avatar from '../Avatar';
+import SettingsModal from './SettingsModal';
 
 interface DesktopSidebarProps {
   currentUser: User;
@@ -14,31 +15,38 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between">
-      <nav className="mt-4 flex flex-col justify-between">
-        <ul role="list" className="flex flex-col items-center space-y-1">
-          {routes.map((route) => (
-            <DesktopSidebarItem
-              key={route.label}
-              href={route.href}
-              label={route.label}
-              icon={route.icon}
-              active={route.active}
-              onClick={route.onClick}
-            />
-          ))}
-        </ul>
-      </nav>
+    <>
+      <SettingsModal
+        currentUser={currentUser}
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+      />
+      <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between">
+        <nav className="mt-4 flex flex-col justify-between">
+          <ul role="list" className="flex flex-col items-center space-y-1">
+            {routes.map((route) => (
+              <DesktopSidebarItem
+                key={route.label}
+                href={route.href}
+                label={route.label}
+                icon={route.icon}
+                active={route.active}
+                onClick={route.onClick}
+              />
+            ))}
+          </ul>
+        </nav>
 
-      <nav className="mt-4 flex flex-col justify-between items-center">
-        <div
-          onClick={() => setIsOpen(true)}
-          className="cursor-pointer hover:opacity-75 transition"
-        >
-          <Avatar user={currentUser} />
-        </div>
-      </nav>
-    </div>
+        <nav className="mt-4 flex flex-col justify-between items-center">
+          <div
+            onClick={() => setIsOpen(true)}
+            className="cursor-pointer hover:opacity-75 transition"
+          >
+            <Avatar user={currentUser} />
+          </div>
+        </nav>
+      </div>
+    </>
   );
 };
 export default DesktopSidebar;

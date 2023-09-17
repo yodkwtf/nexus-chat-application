40285369c3dd331e58f92cf39d58da00d8252d19
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2023-09-17 21:27:20 +0530

    fix

--- app/actions/getSession.ts
+++ app/actions/getSession.ts
@@ -1,6 +1,6 @@
 import { getServerSession } from 'next-auth';
 
-import { authOptions } from '../api/auth/[nextauth]/route';
+import { authOptions } from '../api/auth/[...nextauth]/route';
 
 export default async function getSession() {
   const session = await getServerSession(authOptions);

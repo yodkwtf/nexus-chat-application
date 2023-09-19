6e6874da280365a9091b04d62cb9dd2d4c2fe320
yodkwtf <48durgesh.kumar@gmail.com>
2023-09-19 23:27:05 +0530

    home page UI improved

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,26 +1,49 @@
-import Image from 'next/image';
 import AuthForm from './components/AuthForm';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <Image
-          alt="logo"
-          className="mx-auto w-auto"
-          height={64}
-          width={64}
-          src="/images/logo.png"
-        />
-
-        <h2 className="mt-6 text-center text-3xl font-bold text-gray-900 tracking-tight">
-          Nexus!
-        </h2>
-        <h3 className="mt-4 text-center">Your Ultimate Chat Experience</h3>
+    <main className="flex flex-col-reverse lg:flex-row min-h-screen">
+      {/* Left Column (CTA) */}
+      <div
+        className="lg:w-1/2 p-10"
+        style={{ background: 'linear-gradient(45deg, #00BFFF, #0099CC)' }}
+      >
+        <div className="flex flex-col justify-center h-full">
+          <h1 className="text-4xl font-bold mb-2 text-center text-white">
+            Welcome to Nexus Chat!
+          </h1>
+          <p className="text-lg my-6 mx-auto text-white">
+            Your Ultimate Chat Experience
+          </p>
+          <p className="text-sm opacity-90 mx-auto text-white">
+            Join Nexus Chat today and connect with others effortlessly.
+          </p>
+          <button className="mt-8 bg-white text-cyan-500 hover:bg-cyan-700 hover:text-white py-2 px-8 rounded-full font-semibold shadow-md transition duration-300 ease-in-out w-auto max-w-lg mx-auto">
+            Get Started
+          </button>
+        </div>
       </div>
 
-      {/* Auth Form */}
-      <AuthForm />
+      {/* Right Column (Form) */}
+      <div className="lg:w-1/2 bg-gray-100 p-10">
+        <div className="sm:mx-auto flex flex-col justify-center h-full sm:w-full sm:max-w-md">
+          <Image
+            alt="logo"
+            className="mx-auto w-auto"
+            height={64}
+            width={64}
+            src="/images/logo.png"
+          />
+
+          <h2 className="mt-6 text-center text-3xl font-bold text-gray-900 tracking-tight">
+            Join Nexus Chat Today!
+          </h2>
+
+          {/* Auth Form */}
+          <AuthForm />
+        </div>
+      </div>
     </main>
   );
 }
